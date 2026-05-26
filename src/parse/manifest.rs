05c736251f700fcb15d83f62e0c1dec41304dbb2
Sam Weldon <samweldon9393@gmail.com>
2026-05-26 17:42:05 -0400

    mostly parse manifest file, getting the data file path

---
+++ src/parse/manifest.rs
@@ -0,0 +1,44 @@
+use std::hash::Hash;
+
+use apache_avro::{Reader, from_value, types::Value};
+use serde::{Deserialize};
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ManifestFile {
+    pub status: i32,
+    pub snapshot_id: i64,
+    pub sequence_number: Option<i64>,
+    pub file_sequence_number: Option<i64>,
+    pub data_file: DataFileRecord,
+
+    pub key_metadata: Option<String>,
+    pub split_offsets: Option<String>,
+    pub equality_ids: Option<String>,
+    pub sort_order_id: Option<i64>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct DataFileRecord {
+    pub content: i32,
+    pub file_path: String,
+    pub file_format: String,
+    //pub partition_data: String,
+    pub record_count: i64,
+    pub file_size_in_bytes: i64,
+    // pub column_sizes: Option<String>,
+}
+
+impl ManifestFile {
+    pub fn from_file(path: &str) -> ManifestFile {
+        let file = std::fs::File::open(path)
+            .expect("Failed to open manifest file");
+        let reader = Reader::new(file)
+            .expect("Failed to create Avro reader");
+        let record = reader.into_iter().next()
+            .expect("No records found in manifest file")
+            .expect("Failed to read record from Avro file");
+        from_value(&record)
+            .expect("Failed to parse record")
+    }
+
+}  
